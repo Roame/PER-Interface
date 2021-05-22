d0842f1179c59a2b79fdf1a12212ddffb97ea479
Roame Hildebrand <roamehildebrand@gmail.com>
2021-05-22 13:56:21 -0400

    Minor changes to CMakeLists.txt

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
+#include "math/test.h"
 
 int main(int argc, char **argv) {
     std::cout << "Hello World" << std::endl;
     if(argc > 1) std::cout << argv[1] << std::endl;
+    test myTest;
 }
---
+++ src/math/test.cpp
@@ -0,0 +1,9 @@
+#include "test.h"
+
+#include <iostream>
+
+test::test(){
+    std::cout << "Just a minor change again" << std::endl;
+}
+
+test::~test(){}
