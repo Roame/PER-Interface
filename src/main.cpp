f11f4fb98da897c6d5899a37da75a5c40a056bec
Roame <roamehildebrand@gmail.com>
2021-06-10 14:10:43 -0400

    Updated for windows

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,10 @@
 #include <iostream>
 // #include <fstream>
+#include <windows.h>
 
-#include "graphics/GraphicsManager.h"
 #include "math/Matrix2D.h"
 #include "graphics/Texture.h"
 
 int main(int argc, char **argv) {
-    GraphicsManager graphics(argc, argv);
-
-    int width = 1024, height = 768;
-
-    Texture tex(width, height);
-    std::cout << tex.width << " " << tex.height << std::endl;
-
-    for(int i = 0; i < width; i++){
-        for(int j = 0; j < height; j++){
-            int val = (int) (((float)i/(width-1)+(float)j/(height-1))/2*255);
-            tex.setPixel(i, j, 0, val, 0, 255);
-        }
-    }
-
-    graphics.loadTexture(tex);
-
-    while(1){
-        graphics.update();
-    }
+    
 }
